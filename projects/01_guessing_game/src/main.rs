143d35db8cc50630310ee8f166dcc4a66de95aa5
Renner Poveda <rekefa@gmail.com>
2026-05-17 21:38:38 +1200

    completing lessons and first project

--- projects/01_guessing_game/src/main.rs
+++ projects/01_guessing_game/src/main.rs
@@ -2,12 +2,35 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let secret = 42;
+    let secret = rand::random_range(1..=100);
     let mut attempts = 0;
+    let mut guesses: Vec<u32> = vec![];
 
     println!("Guess the number from 1 to 100.");
+    println!("How many attempts do you want?");
+
+    let max_attempts = loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line");
+
+        match input.trim().parse() {
+            Ok(attempts) => break attempts,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                continue;
+            }
+        };
+    };
 
     loop {
+
+        if attempts > max_attempts {
+            println!("Max numbers of attempts reached, try again!");
+            break;
+        }
+
         println!("Enter a guess:");
 
         let mut guess = String::new();
@@ -24,6 +47,7 @@ fn main() {
             }
         };
 
+        guesses.push(guess);
         attempts += 1;
 
         // cmp returns an Ordering enum: Less, Greater, or Equal.
@@ -32,6 +56,7 @@ fn main() {
             Ordering::Greater => println!("Too large."),
             Ordering::Equal => {
                 println!("Correct in {attempts} attempts.");
+                println!("Previous guesses: {guesses:?}");
                 break;
             }
         }
